c02a342b91229e553707090adc556d4b0f9df2be
Ritesh Benjwal <riteshbenjwal7@gmail.com>
2021-11-01 21:50:58 +0530

    TESTING

--- testback/index.js
+++ testback/index.js
@@ -10,4 +10,20 @@ app.get('/sign-in', (req,res) => res.send("Sign in page"));
 
 app.get('/sign-up', (req,res)=> res.send("Sign-up Page"))
 
+
+const admin = (req,res) => {
+    return res.send("This is Admin Page");
+ }
+
+const isAdmin = (req,res, next) => {
+    console.log("isAdmin is running");
+    next();
+}
+
+const isLoggedIn = (req,res, next) => {
+    console.log("isLogged in running");
+    next();
+}
+app.get("/admin", isAdmin, admin)
+
 app.listen(port, () => console.log(`Example app listening on port ${port}`));                  
