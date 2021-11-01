f17caeb61cb830a1a527fb384909ae3a410ffdb1
Ritesh Benjwal <riteshbenjwal7@gmail.com>
2021-11-01 21:50:28 +0530

    ROUTES_CONTROLLER_MIDDLEWARE_SAVING_USER_DATE

--- projbackend/app.js
+++ projbackend/app.js
@@ -1,20 +1,40 @@
 require("dotenv").config();
+
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
+const authRoutes = require("./routes/auth");
+
+//DB Connection
 mongoose
   .connect(process.env.DATABASE, {
     useNewUrlParser: true,
-    useUnfiedTopology: true,
-    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
   })
   .then(() => {
     console.log("DB CONNECTED");
   });
 
+//Middlewares
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(cookieParser());
+app.use(cors());
+
+//My Routes
+app.use("/api", authRoutes);
+
+//PORT
 const port = process.env.PORT || 8000;
 
+//Starting a server
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`app is running at ${port}`);
 });
--- projbackend/models/user.js
+++ projbackend/models/user.js
@@ -1,85 +1,74 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-const crypto = require('crypto');
-const uuidv1 = require('uuid/v1');
+var mongoose = require("mongoose");
+const crypto = require("crypto");
+const uuidv1 = require("uuid/v1");
 
-
-const blogSchema = new Schema({
+var userSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true,
-        maxLength: 32,
-        trim: true,
+      type: String,
+      required: true,
+      maxlength: 32,
+      trim: true
     },
-
-    lastName: {
-        type: String,
-        maxLength: 32,
-        trim: true,
+    lastname: {
+      type: String,
+      maxlength: 32,
+      trim: true
     },
-
     email: {
-        type: String,
-        trim: true,
-        required: true,
-        unique: true,
+      type: String,
+      trim: true,
+      required: true,
+      unique: true
     },
-
     userinfo: {
-        type: String,
-        trim: true,
+      type: String,
+      trim: true
     },
-
-
     encry_password: {
-        type: String,
-        required: true,
-
+      type: String,
+      required: true
     },
-
     salt: String,
-
     role: {
-        type: Number,
-        default: 0,
+      type: Number,
+      default: 0
     },
-
     purchases: {
-        type: Array,
-        default: [],
-    },
-}, {timeStamps: true});
+      type: Array,
+      default: []
+    }
+  },
+  { timestamps: true }
+);
 
 userSchema
   .virtual("password")
-  .set(function(password){
-      this._password = password;
-      this.salt = uuidv1();
-      this.encry_password = this.securePassword(password);
+  .set(function(password) {
+    this._password = password;
+    this.salt = uuidv1();
+    this.encry_password = this.securePassword(password);
   })
-  .get(function(){
-      return this._password;
-  })
+  .get(function() {
+    return this._password;
+  });
 
-userSchema.method = {
-    authenticate: function(plainpassword){
-        return this.securePassword(plainpassword) === this.encry_password;
-    },
-     
-    securePassword = function (plainpassword) {
-        if (!plainpassword) return "";
+userSchema.methods = {
+  autheticate: function(plainpassword) {
+    return this.securePassword(plainpassword) === this.encry_password;
+  },
 
-        try {
-
-            return crypto.createHmac('sha256', this.salt).update(plainpassword).digest('hex');
-
-        } catch (err) {
-            return "";
-        }
-
+  securePassword: function(plainpassword) {
+    if (!plainpassword) return "";
+    try {
+      return crypto
+        .createHmac("sha256", this.salt)
+        .update(plainpassword)
+        .digest("hex");
+    } catch (err) {
+      return "";
     }
-}
-
-
+  }
+};
 
 module.exports = mongoose.model("User", userSchema);
