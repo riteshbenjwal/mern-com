9c2a9156439933e3c2351f4a6d1ab64605c28664
Ritesh Benjwal <riteshbenjwal7@gmail.com>
2021-11-02 20:51:24 +0530

    MIDDLEWARE_INITIAL_ROUTES

--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,11 +1,23 @@
 var express = require("express");
 var router = express.Router();
-const {signout, signup} = require("../controllers/auth");
+const { check, validationResult } = require("express-validator");
+const { signout, signup } = require("../controllers/auth");
 
-
-
-router.post("/signup", signup);
-
+router.post(
+  "/signup",
+  [
+    check("name")
+      .isLength({ min: 2 })
+      .withMessage("name should be at least 2 char because we have om"),
+    check("email").isEmail().withMessage("email is required"),
+    check("password")
+      .isLength({
+        min: 3,
+      })
+      .withMessage("password should be at least 3 char"),
+  ],
+  signup
+);
 router.get("/signout", signout);
 
 module.exports = router;
