879f4a565058f88e66c51bd372d9866786f3f755
Ritesh Benjwal <riteshbenjwal7@gmail.com>
2021-11-09 20:37:15 +0530

    BUG_FIXED_IN_DB_CATEGORY_DONE

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -15,11 +15,14 @@ exports.signup = (req, res) => {
 
   const user = new User(req.body);
   user.save((err, user) => {
+
     if (err) {
+  
       return res.status(400).json({
         err: "NOT able to save user in DB",
       });
-    }
+}
+
     res.json({
       name: user.name,
       email: user.email,
