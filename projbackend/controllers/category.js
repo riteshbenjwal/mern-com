a4e62f03f6f9320c4dd80d94fbe00d95a92a4693
Ritesh Benjwal <riteshbenjwal7@gmail.com>
2021-11-11 12:45:56 +0530

    PRODUCT_ADD_TO_DB_DONE

--- projbackend/controllers/category.js
+++ projbackend/controllers/category.js
@@ -69,3 +69,4 @@ exports.removeCategory = (req, res) => {
     });
   });
 };
+
--- projbackend/models/product.js
+++ projbackend/models/product.js
@@ -1,48 +1,44 @@
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
 const { ObjectId } = mongoose.Schema;
 
-const productSchema = new Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: true,
-    maxLength: 32,
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: 32
+    },
+    description: {
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: 2000
+    },
+    price: {
+      type: Number,
+      required: true,
+      maxlength: 32,
+      trim: true
+    },
+    category: {
+      type: ObjectId,
+      ref: "Category",
+      required: true
+    },
+    stock: {
+      type: Number
+    },
+    sold: {
+      type: Number,
+      default: 0
+    },
+    photo: {
+      data: Buffer,
+      contentType: String
+    }
   },
+  { timestamps: true }
+);
 
-  description: {
-    type: String,
-    trim: true,
-    required: true,
-    maxLength: 1000,
-  },
-
-  price: {
-    type: Number,
-    required: true,
-    maxLength: 32,
-    trim: true,
-  },
-
-  category: {
-    type: ObjectId,
-    ref: "Category", // referencer from Category model
-    required: true,
-  },
-
-  stock: {
-    type: Number,
-  },
-
-  sold: {
-    type: Number,
-    default: 0,
-  },
-
-  photo: {
-    data: Buffer,
-    contentType: String,
-  },
-});
-
-module.export = mongoose.model("Product", productSchema);
+module.exports = mongoose.model("Product", productSchema);
