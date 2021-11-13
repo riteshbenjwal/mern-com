d214fa2cde0281ae7774f2cc05dff980b6d4b708
Ritesh Benjwal <riteshbenjwal7@gmail.com>
2021-11-13 13:48:28 +0530

    AUTHENTICATION_FRONTEND_DONE

--- projfrontend/src/core/Base.js
+++ projfrontend/src/core/Base.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Menu from "./Menu";
 
 const Base = ({
   title = "My Title",
@@ -8,8 +9,9 @@ const Base = ({
 }) => {
   return (
     <div>
+      <Menu />
       <div className="container-fluid">
-        <div className="jumbotron bg-dark text-white text-center">
+        <div className="jumbotron p-1 bg-dark text-white text-center">
           <h2 className="display-4">{title}</h2>
           <p className="lead">{description}</p>
         </div>
